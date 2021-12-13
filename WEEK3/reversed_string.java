2c9f05710b2489616b48ba5f5e18ee3a4e0789ea
Lisa VC <75480734+LisaVCherian@users.noreply.github.com>
2021-12-13 11:31:06 +0530

    Create reversed_string.java

---
+++ WEEK3/reversed_string.java
@@ -0,0 +1,22 @@
+//Write a Java program to reverse a given string
+
+import java.io.*;
+import java.util.*;
+class reversed_string{
+
+	public static void main(String args[]){
+
+	Scanner sc = new Scanner(System.in);
+	String initial, rev="";
+
+	System.out.print("\nEnter a string: "); 
+	initial = sc.nextLine();
+
+	int length = initial.length();
+
+	for(int i = length-1; i>=0; i--)
+		rev=rev+initial.charAt(i);
+
+	System.out.println("Reversed string: "+rev); 
+	}
+}
